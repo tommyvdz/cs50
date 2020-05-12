075dddaba6b93376d897bb520bb2519643cc104d
Tommy van de Zande <tommyvandezande@gmail.com>
2020-05-12 21:16:17 +0200

    finished pset4

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 typedef uint8_t BYTE;
 
-void findImages(FILE* imagefile);
+int BLOCKSIZE = 512;
+
+void findImages(FILE *imagefile);
 
 int main(int argc, char *argv[])
 {
@@ -24,33 +27,53 @@ int main(int argc, char *argv[])
         return 1;
     }
 
+    //find the actual JPEGs in the given file
     findImages(imagefile);
 
+    //close the current file
     fclose(imagefile);
     return 0;
 
 }
 
-void findImages(FILE* imagefile)
+void findImages(FILE *imagefile)
 {
-    BYTE block[512];
+    BYTE block[BLOCKSIZE];
     int filecount = 0;
-    char filename[8] ={};
+    char filename[8] = {};
+    bool openFile = false;
+    FILE *img;
 
-    while (fread(block, 512, 1, imagefile) == 1)
+    while (fread(block, BLOCKSIZE, 1, imagefile) == 1)
     {
-         // Check first three bytes for pattern and look at first four bits of 4th byte
+        // Check first three bytes for pattern and look at first four bits of 4th byte
         if (block[0] == 0xff && block[1] == 0xd8 && block[2] == 0xff && (block[3] & 0xf0) == 0xe0)
         {
-            // implement fwrite and find a way to continue writing the next block until a new start of a jpeg is found
-            sprintf(filename, "%.3i.jpg", filecount);
-            FILE *img = fopen(filename,"w");
-            filecount++;
+            if (openFile) //if already working on a file, close that file and open a new one
+            {
+                fclose(img);
+                filecount++;
+                sprintf(filename, "%.3i.jpg", filecount);
+                img = fopen(filename, "w");
+                fwrite(block, 1, 512, img);
+            }
+            else //open a new file
+            {
+                sprintf(filename, "%.3i.jpg", filecount);
+                img = fopen(filename, "w");
+                openFile = true;
+                fwrite(block, 1, 512, img);
+            }
+
         }
-        else
+        else //if no JPEG block is found, check if we are currently writing to a file, then continue writing. If not, do nothing
         {
-            printf("No\n");
+            if (openFile)
+            {
+                fwrite(block, 1, 512, img);
+            }
         }
     }
-
 }
+
+
